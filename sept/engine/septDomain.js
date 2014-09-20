cd1d44f0eaea6004475a8484f0ef8614a426e220
orcwarrior <orcwarrior@o2.pl>
2014-09-20 23:57:03 +0200

    Fully functional login request (on view), fixes in request element, domain created.

--- sept/engine/septDomain.js
+++ sept/engine/septDomain.js
@@ -7,34 +7,34 @@
     var domain = {
         /* Message */
         /* Request */
-        RESTRequest       : function (RESTRequestHeaders, content) {
+        RESTRequest           : function (RESTRequestHeaders, content) {
             this.headers = RESTRequestHeaders;
             this.content = content;
         },
-        RESTRequestHeaders: function (RESTOperation, session) {
+        RESTRequestHeaders    : function (RESTOperation, session) {
             this.operation = RESTOperation;
             this.session = session;
         },
         /* Response */
-        RESTResponse      : function (RESTResponseHeaders, content) {
+        RESTResponse          : function (RESTResponseHeaders, content) {
             this.headers = RESTResponseHeaders;
             this.content = content;
         },
-        RESTResponseHeaders : function(RESTResponseStatus, session) {
+        RESTResponseHeaders   : function (RESTResponseStatus, session) {
             this.status = RESTResponseStatus;
             this.session = session;
         },
-        RESTResponseStatus : function(type, message) {
+        RESTResponseStatus    : function (type, message) {
             this.type = type;
             this.message = message;
         },
         /* Entity objects */
-        session           : function (uuid, user, created) {
+        session               : function (uuid, user, created) {
             this.uuid = uuid;
             this.user = user;
             this.created = created;
         },
-        user              : function (id, username, password, email, userRoles, enabled) {
+        user                  : function (id, username, password, email, userRoles, enabled) {
             this.id = id;
             this.username = username;
             this.password = password;
@@ -42,29 +42,29 @@
             this.userRoles = userRoles;
             this.enabled = enabled;
         },
-        userRoles         : function (arrRoles) {
+        userRoles             : function (arrRoles) {
             this.roles = arrUserRoles;
         },
         /* 'enums' values */
-        RESTRequestOperation : [
-            'RESTOperation.login',
-            'RESTOperation.logout',
-            'RESTOperation.register',
-            'RESTOperation.getRoles',
-            'RESTOperation.getArticles',
-            'RESTOperation.getArticleFull',
-            'RESTOperation.getArticleComments',
-            'RESTOperation.favoriteArticle',
-            'RESTOperation.editArticle',
-            'RESTOperation.deleteArticle',
-            'RESTOperation.createArticle',
-            'RESTOperation.addComment',
-            'RESTOperation.deleteComment'
-        ],
-        RESTResponseStatusType : [
-            'RESTResponseStatusType.success',
-            'RESTResponseStatusType.fail'
-        ]
+        RESTRequestOperation  : {
+            'login'             : 'RESTOperation.login',
+            'logout'            : 'RESTOperation.logout',
+            'register'          : 'RESTOperation.register',
+            'getRoles'          : 'RESTOperation.getRoles',
+            'getArticles'       : 'RESTOperation.getArticles',
+            'getArticleFull'    : 'RESTOperation.getArticleFull',
+            'getArticleComments': 'RESTOperation.getArticleComments',
+            'favoriteArticle'   : 'RESTOperation.favoriteArticle',
+            'editArticle'       : 'RESTOperation.editArticle',
+            'deleteArticle'     : 'RESTOperation.deleteArticle',
+            'createArticle'     : 'RESTOperation.createArticle',
+            'addComment'        : 'RESTOperation.addComment',
+            'deleteComment'     : 'RESTOperation.deleteComment'
+        },
+        RESTResponseStatusType: {
+            'success': 'RESTResponseStatusType.success',
+            'fail'  : 'RESTResponseStatusType.fail'
+        }
     };
     _.extend(septEngine.domain, domain);
 }
--- sept/engine/septRequestHelper.js
+++ sept/engine/septRequestHelper.js
@@ -6,8 +6,19 @@
     var requestHelper = {
         /* Message */
         /* Request */
+        'RESTOperation.login' : {
+            processRequestModel: function (model) {
+                if (typeof model.password !== 'undefined') {
+                    model.password = CryptoJS.MD5(model.password).toString();
+                }
+                return model;
+            },
+            initModel          : function () {
+                return new septEngine.domain.user();
+            }
+        }
     };
-    _.extend(septEngine.domain, domain);
+    _.extend(septEngine.requestHelper, requestHelper);
 }
     )
 (
